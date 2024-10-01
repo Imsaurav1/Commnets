2b68068696e79f79f4b7891fd09f042ddb9ba5e5
Saurabh Kumar Jha <100080003+Imsaurav1@users.noreply.github.com>
2024-10-02 02:27:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
+
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-// Connect to MongoDB
-mongoose.connect('mongodb+srv://skjha9th:Saurabhjha@cluster0.ewg8cxv.mongodb.net/Comments?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://skjha9th:Saurabhjha@cluster0.ewg8cxv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const CommentSchema = new mongoose.Schema({
   name: String,
-  comment: String
-});
+  comment: String,
+}, { timestamps: true });
 
 const Comment = mongoose.model('Comment', CommentSchema);
 
@@ -28,6 +29,20 @@ app.post('/comments', async (req, res) => {
   const newComment = new Comment(req.body);
   await newComment.save();
   res.json(newComment);
+});
+
+// Delete a comment
+app.delete('/comments/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedComment = await Comment.findByIdAndDelete(id);
+    if (!deletedComment) {
+      return res.status(404).json({ message: 'Comment not found' });
+    }
+    res.status(200).json(deletedComment);
+  } catch (error) {
+    res.status(500).json({ message: 'Error deleting comment' });
+  }
 });
 
 const port = process.env.PORT || 5000;
