cc68ed5ccee6a181ae31085910e6034da158dc41
Saurabh Kumar Jha <100080003+Imsaurav1@users.noreply.github.com>
2024-10-02 01:44:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,55 +1,36 @@
-// server.js
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-const app = express();
-const PORT = 3000;
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://skjha9th:Saurabhjha@cluster0.ewg8cxv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true });
 
-// Middleware
+const CommentSchema = new mongoose.Schema({
+  name: String,
+  comment: String
+});
+
+const Comment = mongoose.model('Comment', CommentSchema);
+
+const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-// Connect to MongoDB using the local connection URL
-mongoose.connect('mongodb://localhost:27017/commentsDB', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
-    console.log('Connected to MongoDB');
-}).catch((err) => {
-    console.error('Error connecting to MongoDB:', err);
-});
-
-// Define a Comment schema
-const commentSchema = new mongoose.Schema({
-    name: String,
-    comment: String,
-});
-
-const Comment = mongoose.model('Comment', commentSchema);
-
-// Routes
+// Get all comments
 app.get('/comments', async (req, res) => {
-    try {
-        const comments = await Comment.find();
-        res.json(comments);
-    } catch (err) {
-        res.status(500).send(err);
-    }
+  const comments = await Comment.find();
+  res.json(comments);
 });
 
+// Post a comment
 app.post('/comments', async (req, res) => {
-    const { name, comment } = req.body;
-    const newComment = new Comment({ name, comment });
-    try {
-        await newComment.save();
-        res.status(201).json(newComment);
-    } catch (err) {
-        res.status(500).send(err);
-    }
+  const newComment = new Comment(req.body);
+  await newComment.save();
+  res.json(newComment);
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
